d0991d9c69a356267f53c6c8be912d036326c2f3
rizkygusna <gusnafarid@gmail.com>
2022-01-20 12:24:22 +0700

    add fonts

--- src/components/SideBar/component.js
+++ src/components/SideBar/component.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './styles.scss';
+import browse from '../../images/browse.png';
+import merlion from '../../images/merlion.png';
+import videos from '../../images/videos.png';
+import blog from '../../images/blog.png';
+import about from '../../images/about.png';
 
 const SideBar = () => {
   return (
     <div className='sidebar'>
-      <h1>This is a Sidebar</h1>
+      <a className='active' href='#'>
+        <img src={browse} alt='earth' /> Browse
+      </a>
+      <a href='#'>
+        <img src={merlion} alt='merlion' /> Suggest <br /> Attraction
+      </a>
+      <a href='#'>
+        <img src={videos} alt='videos' /> Videos
+      </a>
+      <a href='#'>
+        <img src={blog} alt='blog' /> Blog
+      </a>
+      <a href='#'>
+        <img src={about} alt='about' /> About
+      </a>
     </div>
   );
 };
