12067bcc5e5f736a18644ab053ca966084399809
rizkygusna <gusnafarid@gmail.com>
2022-02-12 19:27:07 +0700

    fix bound center value for map

--- src/components/LocationMap/component.js
+++ src/components/LocationMap/component.js
@@ -1,52 +1,75 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet';
+import { LatLngBounds } from 'leaflet';
 
 import './styles.scss';
 
 const LocationMap = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [locations, setLocations] = useState([]);
+  const [locations, setLocations] = useState(null);
+  const [boundCenter, setBoundCenter] = useState(null);
 
   const url =
     'https://my-json-server.typicode.com/rizkygusna/singapore-attractions/locations';
 
+  const findBoundCenter = (coordinates) => {
+    // find the smallest latitude
+    const smallestLat = [...coordinates].reduce((currLoc, nextLoc) => {
+      if (nextLoc.latitude < currLoc.latitude) {
+        return nextLoc;
+      } else {
+        return currLoc;
+      }
+    });
+
+    //find the biggest latitude
+    const biggestLat = [...coordinates].reduce((currLoc, nextLoc) => {
+      if (nextLoc.latitude > currLoc.latitude) {
+        return nextLoc;
+      } else {
+        return currLoc;
+      }
+    });
+
+    //initiate bound with LatLng type
+    const bounds = new LatLngBounds(
+      [smallestLat.latitude, smallestLat.longitude],
+      [biggestLat.latitude, biggestLat.longitude]
+    );
+
+    //get center of the bounds
+    const boundCenter = bounds.getCenter();
+    return boundCenter;
+  };
+
   useEffect(() => {
     async function fetchData() {
-      //fetch url
-      await fetch(url)
-        .then((res) => {
-          //if response is not okay
-          if (!res.ok) {
-            setIsLoaded(true);
-            setError('Error 404 data not found');
-            // if response is ok convert data to json
-          } else return res.json();
-        })
-        //assign the converted data to locations state
-        .then(
-          (data) => {
-            setIsLoaded(true);
-            setLocations(data);
-          },
-          //error handling
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        );
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        const boundCenter = findBoundCenter(data);
+        setBoundCenter(boundCenter);
+        setIsLoaded(true);
+        setLocations(data);
+        setError(null);
+      } catch (error) {
+        setError(error);
+        console.log("Couldn't fetch data", error);
+      }
     }
     fetchData();
-  }, [url]);
+  }, []);
 
   return (
     <div className='location-map'>
       {error && <h2>{error}</h2>}
       {!isLoaded && <h2>Loading data..</h2>}
-      {locations && (
+      {locations && boundCenter && (
         <MapContainer
-          center={[1.28692, 103.85457]}
+          //bound center will be used here
+          center={boundCenter}
           zoom={15}
           scrollWheelZoom={false}
           zoomControl={false}
